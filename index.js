0ebacc4a4d4332cbe703c2043bec2dbecaaa6564
hafssa <hafssaelbouhli@gmail.com>
2025-03-15 21:08:42 +0000

    first commit

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB Atlas 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 .then(() => console.log("✅ MongoDB Atlas Connected"))
 .catch(err => console.error("❌ MongoDB Connection Error:", err));
 
